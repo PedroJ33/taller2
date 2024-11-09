7dcef744d9318572919addd70af8516f38c8c824
PedroJ33 <pedro.lopez@correounivalle.edu.co>
2024-11-08 21:02:21 -0500

    Taller2

--- app/src/test/scala/taller/AppSuite.scala
+++ app/src/test/scala/taller/AppSuite.scala
@@ -9,7 +9,5 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends AnyFunSuite {
-  test("App has a greeting") {
-    assert(App.greeting() != null)
-  }
+
 }
